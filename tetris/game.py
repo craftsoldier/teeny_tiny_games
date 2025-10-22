f051426b725762657a7555a2d5fe38358371a5a0
Julian Abraham <julian.abraham@xaviers.edu.in>
2025-10-22 22:06:07 +0530

    working tetris

--- tetris/game.py
+++ tetris/game.py
@@ -1,5 +1,4 @@
-#this is tetris btw
-
+import os
 import random
 
 class Piece:
@@ -14,20 +13,23 @@ class Piece:
                               't': [(-1, 0), (0, -1), (0, 0), (0, 1)]
                              }
        
-    def rotate(self, tetromino, angle):
-        rotation_list = self.piece_coords[tetromino]
-        new_coords = []
-        for (x, y) in rotation_list:
-            if angle == 1: 
+    def rotate(self, tetromino, original_coords, direction):
+        if direction == 1:
+            new_coords  = []
+            for (x, y) in original_coords:
                 new_coord = (y, -x)
-            elif angle == -1:
+                new_coords.append(new_coord)
+
+        elif direction == -1:
+            new_coords = []
+            for (x, y) in original_coords:
                 new_coord = (-y, x)
-            new_coords.append(new_coord)
-        rotation_list = new_coords
-        return rotation_list
+                new_coords.append(new_coord)
+        return new_coords
 
     def random_bag(self):
-        bag_pieces = [random.randint(0,6) for i in range(10)]
+        bag_numbers = [random.randint(0,6) for i in range(10)]
+        bag_pieces = [self.piece_list[bag] for bag in bag_numbers]
         return bag_pieces
 
 
@@ -39,20 +41,41 @@ class Board:
         self.pieces = Piece()
         self.current_piece = None
         self.current_position = None
-        self.current_coords = None
+        self.current_coords = []
 
     def spawn_piece(self, piece_type):
+        self.current_piece = piece_type
+        self.current_position = (2, 4)
+        self.current_coords = self.pieces.piece_coords[piece_type]
+
+    def move_down(self, position):
+        return (position[0] + 1, position[1])
+
+    def move_sideways(self, position, direction):
+        if direction > 0:
+            return (position[0], position[1] - 1)
+        elif direction < 0:
+            return (position[0], position[1] + 1)
+
+    def collision_check(self, coords, position):
+        for (x, y) in coords:
+            x_ash = position[0] + x
+            y_ash = position[1] + y
+            if (x_ash < 0 or x_ash >= self.height) or (y_ash < 0 or y_ash >= self.width): 
+                return False
+            if self.grid[x_ash][y_ash] == 1:
+                return False
+        return True
     
-    def display_piece(self, piece, position):
-        if piece in self.pieces.piece_coords:
-            piece_list = self.pieces.piece_coords[piece]
-        piece_place = [(x + position[0], y + position[1]) for x, y in self.pieces.piece_coords[piece]]
-        for pawn in piece_place:    
-            self.grid[pawn[0]][pawn[1]] = 1
-        
+    def display_piece(self, position):
+        for y, x in self.current_coords:    
+            self.grid[position[0] + y][position[1] + x] = 1
+
+    def clear_piece(self, position):
+        for y, x in self.current_coords:
+            self.grid[position[0] + y][position[1] + x] = 0
         
     def display_board(self):
-        print()
         print()
         for i in range(self.height):
             print('|', end='')
@@ -67,16 +90,144 @@ class Board:
             print('--', end='')
         print('--')
 
+    def game_over(self):
+        if not self.collision_check(self.current_coords, self.current_position):
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("GAME OVER")
+            return True
+        return False
+
+
+    def garbage_collector(self):
+        garbage = True
+        while garbage:
+            garbage = False
+            for i in range(self.height):
+                row_full = True
+                for k in range(self.width):
+                    if self.grid[i][k] == 0:
+                        row_full = False
+                        break
+                if row_full:
+                    del self.grid[i]
+                    self.grid.insert(0, [0] * self.width)
+                    garbage = True
+                    break
+
     def play(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
         bags = self.pieces.random_bag()
+        bag_int = 0
 
+        self.spawn_piece(bags[bag_int])
+        self.display_piece(self.current_position)
+        self.display_board()
 
+        move_list = { 'w': 0,
+                      'a': 2, #left
+                      's': 0,
+                      'd': -2, #right
+                      'z': -1, #ccw
+                      'x': 1, #cw
+                      ' ': 999} #hard-drop
 
+        while True:
+            move = input()
+            os.system('cls' if os.name == 'nt' else 'clear')
+            if move in move_list:
 
+                new_position = self.move_down(self.current_position)
+                self.clear_piece(self.current_position)
+                if self.collision_check(self.current_coords, new_position):
+                    self.display_piece(new_position)
+                    self.current_position = new_position 
+                        
+                    if move in ['a', 'd']: #side-ways
+                        side_position = self.move_sideways(new_position, move_list[move])
+                        self.clear_piece(new_position)
+                        if self.collision_check(self.current_coords, side_position):
+                            self.display_piece(side_position)
+                            self.current_position = side_position
+                        else:
+                            self.display_piece(new_position)
+
+                    elif move == 'z' or move == 'x': #rotation
+                        self.clear_piece(self.current_position)
+                        rotate_coords = self.pieces.rotate(self.current_piece, self.current_coords, move_list[move])
+                        if self.collision_check(rotate_coords, self.current_position):
+                            self.current_coords = rotate_coords
+                            self.display_piece(self.current_position)
+                        else:
+                            self.display_piece(self.current_position)
+
+                    elif move == ' ': #hard-drop
+                        while True:
+                            new_position = self.move_down(self.current_position)
+                            self.clear_piece(self.current_position)
+                            if self.collision_check(self.current_coords, new_position):
+                                self.display_piece(new_position)
+                                self.current_position = new_position 
+                            else:
+                                self.display_piece(self.current_position)
+
+                                bag_int += 1
+                                self.garbage_collector()
+                                if bag_int < 10:
+                                    self.spawn_piece(bags[bag_int])
+                                    self.display_piece(self.current_position)
+                                else:
+                                    bags = self.pieces.random_bag()
+                                    bag_int = 0
+                                    self.spawn_piece(bags[bag_int])
+                                    if self.game_over():
+                                        break
+                                    self.display_piece(self.current_position)
+                                break
+
+                else:
+                    self.display_piece(self.current_position)
+
+                    bag_int += 1
+                    self.garbage_collector()
+                    if bag_int < 10:
+                        self.spawn_piece(bags[bag_int])
+                        if self.game_over():
+                            break
+                        self.display_piece(self.current_position)
+                    else:
+                        bags = self.pieces.random_bag()
+                        bag_int = 0
+                        self.spawn_piece(bags[bag_int])
+                        if self.game_over():
+                            break
+                        self.display_piece(self.current_position)
+
+                self.display_board()
+            else:
+                new_position = self.move_down(self.current_position)
+                self.clear_piece(self.current_position)
+                if self.collision_check(self.current_coords, new_position):
+                    self.display_piece(new_position)
+                    self.current_position = new_position
+                else:
+                    self.display_piece(self.current_position)
+                    bag_int += 1
+                    self.garbage_collector()
+                    if bag_int < 10:
+                        self.spawn_piece(bags[bag_int])
+                        if self.game_over():
+                            break
+                        self.display_piece(self.current_position)
+                    else:
+                        bags = self.pieces.random_bag()
+                        bag_int = 0
+                        self.spawn_piece(bags[bag_int])
+                        if self.game_over():
+                            break
+                        self.display_piece(self.current_position)
+
+                self.display_board()
+                continue
 
 game = Board()
-game.display_piece('t', (5,3))
-game.display_board()
-piece = Piece()
-
-print(piece.rotate('t', 1))
+game.play()
